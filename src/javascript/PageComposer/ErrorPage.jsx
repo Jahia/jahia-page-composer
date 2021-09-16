3f7d88be8854066a9e234f0c031da068e68af463
Thomas Draier <tdraier@jahia.com>
2021-09-16 16:14:12 +0200

    BACKLOG-15311: Implement 404 page design

--- src/javascript/PageComposer/ErrorPage.jsx
+++ src/javascript/PageComposer/ErrorPage.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import {Button, Typography} from '@jahia/moonstone';
 import PropTypes from 'prop-types';
 import {useTranslation} from 'react-i18next';
+import styles from './ErrorPage.scss';
+import clsx from 'clsx';
+
 export const ErrorPage = ({onClick}) => {
     const {t} = useTranslation('jahia-page-composer');
 
     return (
-        <div className="flexFluid flexCol_center alignCenter">
-            <div style={{margin: 'var(--spacing-medium)'}}>
-                <Typography variant="heading">{t('errorTitle')}</Typography>
-            </div>
-            <div style={{
-                margin: 'var(--spacing-medium)',
-                textAlign: 'center'
-            }}
-            >
-                <Typography>{t('errorContent1')}<br/>{t('errorContent2')}</Typography>
-            </div>
-            <div style={{margin: 'var(--spacing-medium)'}}>
+        <div className={styles.page404}>
+            <div className={clsx(styles['page404-wrapper'], 'flexCol', 'alignCenter')}>
+                <Typography component="h1"
+                            variant="title"
+                            weight="bold"
+                            className={styles['page404-title']}
+                >{t('errorTitle')}
+                </Typography>
+                <Typography variant="subtitle" className={styles['page404-description']}>{t('errorContent1')}</Typography>
+                <Typography variant="subtitle" className={styles['page404-description']}>{t('errorContent2')}</Typography>
                 <Button isDisabled={!onClick}
                         label={t('backToHome')}
                         color="accent"
                         size="big"
+                        className={styles['page404-button']}
                         onClick={onClick}
                 />
             </div>
