ee505169b1416d6432379bf125ffc3ec72426730
Alex Karmanov <alexandrk_@hotmail.com>
2022-09-13 07:44:39 -0400

    QA-14040 Remove unnecessary wait

--- tests/cypress/e2e/pageCreationTest.cy.ts
+++ tests/cypress/e2e/pageCreationTest.cy.ts
@@ -22,13 +22,9 @@ describe('Page creation tests', () => {
     })
 
     it('Special characters are handled correctly in page name', function () {
-        cy.wait(1000)
         pageComposer.createPage(nameWithSpecialChars);
-        // Wait for frame to load created page fully
         PageComposer.visit(site, 'en', 'home.html');
-        cy.wait(5000);
         pageComposer.navigateToPage(nameWithSpecialChars);
-        cy.wait(500);
         // TODO this will need to be changed to accommodate updated functionality
         cy.get('h1').contains('Page not found');
     })
