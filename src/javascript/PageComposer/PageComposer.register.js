b909d0658e209537944b7ff0d699054c4fd66bcd
Cedric Mailleux <cmailleux@jahia.com>
2023-08-04 12:34:25 -0400

    BACKLOG-21097: Disable legacy page composer(actions,routes,redux) in registry

--- src/javascript/PageComposer/PageComposer.register.js
+++ src/javascript/PageComposer/PageComposer.register.js
@@ -3,8 +3,12 @@ import {pageComposerRoutes} from './PageComposer.routes';
 import {pageComposerRedux} from './PageComposer.redux';
 import {registry} from '@jahia/ui-extender';
 
+const booleanValue = v => typeof v === 'string' ? v === 'true' : Boolean(v);
+
 export default function () {
-    pageComposerRedux();
-    pageComposerRoutes(registry);
-    pageComposerActions(registry);
+    if (!booleanValue(contextJsParameters.config.jcontent?.hideLegacyPageComposer)) {
+        pageComposerRedux();
+        pageComposerRoutes(registry);
+        pageComposerActions(registry);
+    }
 }
