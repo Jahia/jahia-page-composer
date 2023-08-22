f8b99c4e386b761ba6ad58a86409e6e4e307c6eb
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-08-22 14:38:13 +0200

    fix reporter config

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -72,10 +72,14 @@ curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisionin
 echo "$(date +'%d %B %Y - %k:%M')== Run tests =="
 yarn e2e:ci
 if [[ $? -eq 0 ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') == Full execution successful =="
   echo "success" > ./results/test_success
+  yarn report:merge; yarn report:html
   exit 0
 else
+  echo "$(date +'%d %B %Y - %k:%M') == One or more failed tests =="
   echo "failure" > ./results/test_failure
+  yarn report:merge; yarn report:html
   exit 1
 fi
 
