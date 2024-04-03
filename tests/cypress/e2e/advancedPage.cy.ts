505818739151b5e24e623bc6e5026235474a2be6
Francois G <fgerthoffert@jahia.com>
2024-04-03 15:03:38 +0200

    Removed incorrect msg

--- tests/cypress/e2e/advancedPage.cy.ts
+++ tests/cypress/e2e/advancedPage.cy.ts
@@ -120,7 +120,6 @@ describe('Advanced page testsuite', () => {
         deleteNode(`/sites/${siteKey}/home/menu-title`);
     });
 
-    // This test is skipped because it was hanging due to a OWASP warning for the external link
     // To be fixed in BACKLOG-22510
     it.skip('Lock/Clear lock', () => {
         createExternalLink();
