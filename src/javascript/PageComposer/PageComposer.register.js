ba7becedc481aa94856f17edc09d300567deb79e
Geofrey Flores <gflores@jahia.com>
2024-08-12 16:36:30 -0400

    BACKLOG-23091: Remove 'Open in Page Composer' menu action

--- src/javascript/PageComposer/PageComposer.register.js
+++ src/javascript/PageComposer/PageComposer.register.js
@@ -1,4 +1,3 @@
-import {pageComposerActions} from './PageComposer.actions';
 import {pageComposerRoutes} from './PageComposer.routes';
 import {pageComposerRedux} from './PageComposer.redux';
 import {registry} from '@jahia/ui-extender';
@@ -9,6 +8,5 @@ export default function () {
     if (!booleanValue(contextJsParameters.config.jcontent?.hideLegacyPageComposer)) {
         pageComposerRedux();
         pageComposerRoutes(registry);
-        pageComposerActions(registry);
     }
 }
