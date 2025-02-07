13d908619839dd04d07c4990fb74db8914d5794f
akarmanov <alexandrk_@hotmail.com>
2025-02-07 07:10:14 -0500

    BACKLOG-23494: Remove unused target

--- src/javascript/PageComposer/PageComposer.actions.jsx
+++ src/javascript/PageComposer/PageComposer.actions.jsx
@@ -6,6 +6,6 @@ export const pageComposerActions = registry => {
     registry.add('action', 'pageComposer', pageComposerAction, {
         buttonLabel: 'jcontent:label.contentManager.actions.pageComposer',
         buttonIcon: <OpenInBrowser/>,
-        targets: ['contentActions:11', 'narrowHeaderMenu:10.5', 'browseControlBar:12', 'contentItemActions:21', 'contentItemContextActions:22']
+        targets: ['narrowHeaderMenu:10.5', 'browseControlBar:12', 'contentItemActions:21', 'contentItemContextActions:22']
     });
 };
