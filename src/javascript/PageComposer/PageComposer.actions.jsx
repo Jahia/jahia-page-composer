bf8eb1bce9c1bf38c4b35ae48c0d527016756fe5
Geofrey Flores <gflores@jahia.com>
2025-02-13 10:30:57 -0500

    Re-order 'open in page composer' action

--- src/javascript/PageComposer/PageComposer.actions.jsx
+++ src/javascript/PageComposer/PageComposer.actions.jsx
@@ -6,6 +6,7 @@ export const pageComposerActions = registry => {
     registry.add('action', 'pageComposer', pageComposerAction, {
         buttonLabel: 'jcontent:label.contentManager.actions.pageComposer',
         buttonIcon: <OpenInBrowser/>,
-        targets: ['narrowHeaderMenu:10.5', 'browseControlBar:12', 'contentItemActions:21', 'contentItemContextActions:22']
+        // Target just before contentActionsSeparator3 in jContent
+        targets: ['narrowHeaderMenu:49', 'browseControlBar:49', 'contentItemActions:49', 'contentItemContextActions:49']
     });
 };
